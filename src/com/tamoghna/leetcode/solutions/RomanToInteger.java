3c71456287fdc0877970aff6d75c338bc1dc745a
tamoghna-purkait10 <tamoghna10@hotmail.com>
2024-04-23 16:27:20 +0530

    Leetcode solution to roman to integer

---
+++ src/com/tamoghna/leetcode/solutions/RomanToInteger.java
@@ -0,0 +1,36 @@
+package com.tamoghna.leetcode.solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class RomanToInteger {
+	
+    public static int romanToInt(String s) {
+         Map<Character, Integer> map = new HashMap<>();
+
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+
+		int sum = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))) {
+				sum -= map.get(s.charAt(i));
+			} else {
+				sum += map.get(s.charAt(i));
+			}
+
+		}
+		return sum;
+        
+    }
+    
+    public static void main (String[] args) {
+    	System.out.println(romanToInt("XCVIII"));
+    }
+}
