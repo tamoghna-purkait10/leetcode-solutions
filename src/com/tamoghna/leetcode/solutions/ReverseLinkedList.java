f77a043c03eae37848de0fa65302ccc7438caa4f
tamoghna-purkait10 <tamoghna10@hotmail.com>
2024-05-07 18:29:03 +0530

    Leetcode solution to reverse a linked list

---
+++ src/com/tamoghna/leetcode/solutions/ReverseLinkedList.java
@@ -0,0 +1,28 @@
+package com.tamoghna.leetcode.solutions;
+
+public class ReverseLinkedList {
+
+	private static ListNode reverseList(ListNode head) {
+		ListNode newHead = null;
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = next;
+		}
+		return newHead;
+	}
+
+	public static void main(String[] args) {
+
+		ListNode node = new ListNode(1);
+		node.next = new ListNode(2);
+		node.next.next = new ListNode(3);
+		node.next.next.next = new ListNode(4);
+		node.next.next.next.next = new ListNode(5);
+
+		reverseList(node);
+
+	}
+
+}
